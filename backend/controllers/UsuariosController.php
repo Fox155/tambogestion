9f6403e2ae8640aef0a6917aad61ede23666c687
Fox - Net <mau.slgym@gmail.com>
2019-08-28 00:41:35 -0300

    Pseudo Usuarios

---
+++ backend/controllers/UsuariosController.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace backend\controllers;
+
+use common\models\Usuarios;
+use common\models\GestorUsuarios;
+use common\models\GestorTipoUsuario;
+use common\models\forms\BuscarForm;
+// use common\models\forms\AuditoriaForm;
+// use common\components\PermisosHelper;
+use Yii;
+use yii\web\Controller;
+use yii\data\Pagination;
+use yii\helpers\ArrayHelper;
+
+class UsuariosController extends Controller
+{
+    public function actionIndex()
+    {
+        // $busqueda = new BuscarForm();
+
+        // if ($busqueda->load(Yii::$app->request->post()) && $busqueda->validate()) {
+        //     $tipo = $busqueda->Combo ? $busqueda->Combo : 'T';
+        //     $estado = $busqueda->Combo2 ? $busqueda->Combo2 : 'A';
+        //     $clientes = $gestor->Buscar($busqueda->Cadena, $tipo, $estado);
+        // } else {
+        //     $clientes = $gestor->Buscar();
+        // }
+
+        $usuarios = GestorUsuarios::Buscar();
+
+        return $this->render('index', [
+            'models' => $usuarios
+            // 'busqueda' => $busqueda
+        ]);
+    }
+
+    public function actionAlta()
+    {
+        PermisosHelper::verificarPermiso('AltaCliente');
+
+        $cliente = new Clientes();
+
+        $cliente->Tipo = Yii::$app->request->get('Tipo');
+
+        if ($cliente->Tipo == 'F') {
+            $cliente->setScenario(Clientes::_ALTA_FISICA);
+        } else {
+            $cliente->setScenario(Clientes::_ALTA_JURIDICA);
+        }
+
+        $listas = GestorListasPrecio::Buscar();
+
+        if ($cliente->load(Yii::$app->request->post()) && $cliente->validate()) {
+            $gestor = new GestorClientes();
+            $resultado = $gestor->Alta($cliente);
+
+            Yii::$app->response->format = 'json';
+            if (substr($resultado, 0, 2) == 'OK') {
+                return ['error' => null];
+            } else {
+                return ['error' => $resultado];
+            }
+        } else {
+            return $this->renderAjax('alta', [
+                'titulo' => 'Alta Cliente',
+                'model' => $cliente,
+                'listas' => $listas
+            ]);
+        }
+    }
+
+    public function actionEditar($id)
+    {
+        PermisosHelper::verificarPermiso('ModificarCliente');
+        
+        $cliente = new Clientes();
+
+        $clienteAux = new Clientes();
+        $clienteAux->IdCliente = $id;
+        $clienteAux->Dame();
+        if ($clienteAux->Tipo == 'F') {
+            $cliente->setScenario(Clientes::_MODIFICAR_FISICA);
+        } else {
+            $cliente->setScenario(Clientes::_MODIFICAR_JURIDICA);
+        }
+        
+        $listas = GestorListasPrecio::Buscar();
+
+        if ($cliente->load(Yii::$app->request->post()) && $cliente->validate()) {
+            $gestor = new GestorClientes();
+            $resultado = $gestor->Modificar($cliente);
+
+            Yii::$app->response->format = 'json';
+            if ($resultado == 'OK') {
+                return ['error' => null];
+            } else {
+                return ['error' => $resultado];
+            }
+        } else {
+            return $this->renderAjax('alta', [
+                        'titulo' => 'Editar Cliente',
+                        'model' => $clienteAux,
+                        'listas' => $listas
+            ]);
+        }
+    }
+
+    public function actionBorrar($id)
+    {
+        PermisosHelper::verificarPermiso('BorrarCliente');
+
+        Yii::$app->response->format = 'json';
+        
+        $cliente = new Clientes();
+        $cliente->IdCliente = $id;
+
+        $gestor = new GestorClientes();
+
+        $resultado = $gestor->Borrar($cliente);
+
+        if ($resultado == 'OK') {
+            return ['error' => null];
+        } else {
+            return ['error' => $resultado];
+        }
+    }
+
+    public function actionActivar($id)
+    {
+        PermisosHelper::verificarPermiso('ActivarCliente');
+
+        Yii::$app->response->format = 'json';
+        
+        $cliente = new Clientes();
+        $cliente->IdCliente = $id;
+
+        $resultado = $cliente->Activar();
+
+        if ($resultado == 'OK') {
+            return ['error' => null];
+        } else {
+            return ['error' => $resultado];
+        }
+    }
+
+    public function actionDarBaja($id)
+    {
+        PermisosHelper::verificarPermiso('DarBajaCliente');
+
+        Yii::$app->response->format = 'json';
+        
+        $cliente = new Clientes();
+        $cliente->IdCliente = $id;
+
+        $resultado = $cliente->DarBaja();
+
+        if ($resultado == 'OK') {
+            return ['error' => null];
+        } else {
+            return ['error' => $resultado];
+        }
+    }
+}
+
+?>
--- common/config/main.php
+++ common/config/main.php
@@ -1,13 +1,41 @@
 <?php
-return [
+$conf = [
+    'timeZone' => 'America/Argentina/Tucuman',
+    'language' => 'es',
+    'name' => 'Tambo Gestion',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
+        // 'cache' => [
+        //     'class' => 'yii\caching\FileCache',
+        //     'user' => [
+        //         'identityClass' => 'common\models\Usuarios',
+        //         'loginUrl' => '/usuarios/login',
+        //         'authTimeout' => 60 * 60,
+        //     ],
+        // ], 
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '<controller>/<id:\d+>' => '<controller>',
+                '<controller>/<action>/<id:\d+>' => '<controller>/<action>',
+                '<controller>/index' => '<controller>',
+            ],
         ],
     ],
 ];
+
+// Si se encuentra en modo Desarrollo
+if (YII_ENV_DEV) {
+    $conf['bootstrap'][] = 'debug';
+    $conf['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        'allowedIPs' => ['*'],
+    ];
+}
+
+return $conf;
